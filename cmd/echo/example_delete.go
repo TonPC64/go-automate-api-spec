a3f7f07e904bc08779b03b042de680c10165557e
chanwit-p <chanwit.p@linecorp.com>
2025-03-28 00:56:25 +0700

    feat: fix response of delete api

--- cmd/echo/example_delete.go
+++ cmd/echo/example_delete.go
@@ -6,6 +6,10 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type response struct {
+	Message string `json:"message"`
+}
+
 // deleteExampleHandler godoc
 //
 //	@Summary		Delete an example
@@ -13,7 +17,7 @@ import (
 //	@Tags			examples
 //	@Accept			json
 //	@Produce		json
-//	@Success		200	{object}	map[string]string
+//	@Success		200	{object}	response
 //	@Failure		500	{object}	map[string]string
 //	@Router			/examples [delete]
 func deleteExampleHandler(c echo.Context) error {
